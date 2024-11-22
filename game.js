6d87c8d66883fffd4ca425137380501679174907
velo24dl <velo24dl@student.ju.se>
2024-11-22 14:13:17 +0100

    Initial commit
    Basic code for changing screens

--- game.js
+++ game.js
@@ -1,7 +1,42 @@
+let state = "start";
+
 function setup() {
   createCanvas(800, 600);
 }
 
+function startScreen() {
+  background(0, 140, 0);
+}
+
+function gameScreen() {
+  background(0, 140, 255);
+}
+
+function youWinScreen() {
+  background(255, 0, 0);
+}
+function gameOverScreen() {
+  background(255, 0, 140);
+}
+
 function draw() {
-  background(255, 140, 0);
+  if (state === "start") {
+    startScreen();
+  } else if (state === "playGame") {
+    gameScreen();
+  } else if (state === "gameOver") {
+    gameOverScreen();
+  } else if (state === "youWin") {
+    youWinScreen();
+  }
+}
+
+function mouseClicked() {
+  if (state === "start") {
+    state = "playGame";
+  } else if (state === "playGame") {
+    state = "gameOver";
+  } else if (state === "gameOver") {
+    state = "start";
+  }
 }
